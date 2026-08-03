9ceaf2b82f5a4f58d96f555de647ac6c46c5cf9e
Alfaz Bilakhiya <alfazbilakhiya17@gmail.com>
2026-08-03 11:20:50 +0530

    .

--- src/components/public/Footer.tsx
+++ src/components/public/Footer.tsx
@@ -14,6 +14,9 @@ export default function Footer() {
     address: 'Bala Enterprise Factory, Bhavnagar GIDC Industrial Area, Gujarat - 364001, India',
     tradeindiaUrl: 'https://www.tradeindia.com/bala-enterprise-24235777/',
     indiamartUrl: 'https://www.indiamart.com/balaenterprises-gujarat/profile.html?srsltid=AfmBOoo-CME_id8olb_pyMrBd8IurDJTfC_G5k_UzNsao729y8RASlvF',
+    instagramUrl: 'https://www.instagram.com/',
+    youtubeUrl: 'https://www.youtube.com/',
+    linkedinUrl: 'https://www.linkedin.com/',
   });
 
   useEffect(() => {
@@ -29,6 +32,9 @@ export default function Footer() {
             address: d.address || prev.address,
             tradeindiaUrl: d.tradeindia_url || prev.tradeindiaUrl,
             indiamartUrl: d.indiamart_url || prev.indiamartUrl,
+            instagramUrl: d.instagram_url || prev.instagramUrl,
+            youtubeUrl: d.youtube_url || prev.youtubeUrl,
+            linkedinUrl: d.linkedin_url || prev.linkedinUrl,
           }));
         }
       })
@@ -86,8 +92,8 @@ export default function Footer() {
               </span>
             </div>
 
-            {/* Verified B2B Partner Badges */}
-            <div className="flex items-center gap-2.5 pt-2">
+            {/* Verified B2B Partner Badges & Social Media Links */}
+            <div className="flex flex-wrap items-center gap-2.5 pt-2">
               <a
                 href={siteInfo.indiamartUrl}
                 target="_blank"
@@ -118,6 +124,45 @@ export default function Footer() {
                   height={32}
                   className="h-8 w-auto object-contain scale-105"
                 />
+              </a>
+
+              {/* YouTube Icon */}
+              <a
+                href={siteInfo.youtubeUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-10 h-10 flex items-center justify-center bg-[#FF0000] hover:bg-[#cc0000] text-white transition-all duration-300 rounded-md shadow-sm hover:scale-105 shrink-0"
+                title="YouTube Channel"
+              >
+                <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24">
+                  <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+                </svg>
+              </a>
+
+              {/* Instagram Icon */}
+              <a
+                href={siteInfo.instagramUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-10 h-10 flex items-center justify-center bg-gradient-to-tr from-[#f9ce34] via-[#ee2a7b] to-[#6228d7] text-white transition-all duration-300 rounded-md shadow-sm hover:scale-105 shrink-0"
+                title="Instagram"
+              >
+                <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24">
+                  <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.779-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+                </svg>
+              </a>
+
+              {/* LinkedIn Icon */}
+              <a
+                href={siteInfo.linkedinUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-10 h-10 flex items-center justify-center bg-[#0A66C2] hover:bg-[#084e96] text-white transition-all duration-300 rounded-md shadow-sm hover:scale-105 shrink-0"
+                title="LinkedIn"
+              >
+                <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24">
+                  <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.28 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.75M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z"/>
+                </svg>
               </a>
             </div>
           </div>
